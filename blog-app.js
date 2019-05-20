f7d5a5aed604b10894904e787b120954cd1e0073
ffraniel <francis.whitehead@googlemail.com>
2019-05-20 14:22:32 +0100

    copied basic setup of page, added vue to fetch bblog data, created list, added sanity cms

---
+++ blog-app.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+  el: '#blog-list',
+  data: {
+    blogPosts: []
+  },
+  mounted: function () {
+    return fetch ('https://ksrgjbn5.api.sanity.io/v1/data/query/news?query=*[_type == "post"]')
+      .then(resBuff => resBuff.json())
+      .then(res => {
+        let resultWithSnippet = res.result.map(item => {
+          let snippetSplit = item.body[0].children[0].text.split(" ");
+          let snippet;
+          if (snippetSplit.length > 30) {
+            snippet = snippetSplit;
+            snippet.length = 30;
+            snippet.push("...");
+            snippet = snippet.join(" ");
+
+          } else {
+            snippet = snippetSplit.push("...").join(" ");
+          };
+          item.snippet = snippet;
+          item.expanded = false;
+          return item;
+        })
+        this.blogPosts = resultWithSnippet;
+      })
+      .catch(err => {
+        console.log({error: err});
+      })
+  },
+  methods: {
+    expand: function (id) {
+      let newBlogPostsState = this.blogPosts.map((post) => {
+        if (post._id === id) {
+          post.expanded = !post.expanded;
+        }
+        return post;
+      });
+      this.blogPosts = newBlogPostsState;
+    }
+  }
+});
---
+++ blog-script.js
@@ -0,0 +1,75 @@
+const home = 
+  document.getElementById('home') || 
+  document.getElementById('meet-hens') || 
+  document.getElementById('contact-page') ||
+  document.getElementById('blog');
+const meet = document.getElementById('meet');
+const contact = document.getElementById('contact');
+
+const navHome = document.getElementById('nav-home');
+const navMeet = document.getElementById('nav-meet');
+const navContact = document.getElementById('nav-contact');
+
+const linkToHome = (e) => {
+  e.preventDefault();
+  home.classList.add('slide-out');
+  setTimeout(() => {
+    window.location.href = navHome.href;
+  }, 1000);
+};
+
+const linkToMeet = (e) => {
+  e.preventDefault();
+  home.classList.add('slide-out');
+  setTimeout(() => {
+    window.location.href = navMeet.href;
+  }, 1000);
+};
+
+const linkToContact = (e) => {
+  e.preventDefault();
+  home.classList.add('slide-out');
+  setTimeout(() => {
+    window.location.href = navContact.href;
+  }, 1000);
+};
+
+navHome.addEventListener('click', linkToHome);
+
+navMeet.addEventListener('click', linkToMeet);
+
+navContact.addEventListener('click', linkToContact);
+
+//// mobile navigation ////
+const mobNavHome = document.querySelector("#mob-nav-home");
+const mobNavMeet = document.querySelector("#mob-nav-meet");
+const mobNavContact = document.querySelector("#mob-nav-contact");
+
+const mobileToggle = document.querySelector('.mobile-nav-burger');
+const mobileNav = document.querySelector('.mobile-nav');
+const closeNav = document.querySelector('.close-nav');
+const mobileNavLinks = document.querySelectorAll('.mob-nav');
+
+mobileNavLinks.forEach((mobNav) => {
+  mobNav.addEventListener('click', (e)=>{
+    e.preventDefault();
+    mobileNav.classList.toggle('open-nav');
+    setTimeout(() => {
+      if(mobNav.id === 'mob-nav-meet') {
+        window.location.href = 'hens.html';
+      } else if (mobNav.id === 'mob-nav-home') {
+        window.location.href = 'index.html';
+      } else if (mobNav.id === 'mob-nav-contact') {
+        window.location.href = 'contact.html';
+      };
+    }, 500);
+  });
+});
+
+closeNav.addEventListener('click', ()=> {
+  mobileNav.classList.toggle('open-nav');
+})
+
+mobileToggle.addEventListener('click', () => {
+  mobileNav.classList.toggle('open-nav');
+});
