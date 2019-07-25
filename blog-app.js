b986de272bcfc1e52ad47a530d93967d40519c79
ffraniel <francis.whitehead@googlemail.com>
2019-07-25 12:30:36 +0100

    added seperation of blog downloads

--- blog-app.js
+++ blog-app.js
@@ -1,43 +1,14 @@
 var app = new Vue({
   el: '#blog-list',
   data: {
-    blogPosts: []
+    blogPosts: [],
+    totalNumberOfPosts: null,
+    currentFetch: 0
   },
   mounted: function () {
-    return fetch ('https://ksrgjbn5.api.sanity.io/v1/data/query/news?query=*[_type == "post"]| order(_createdAt desc)')
-      .then(resBuff => resBuff.json())
-      .then(res => {
-        let resultWithSnippet = res.result.map(item => {
-          let arrayOfBlocks = item.body;
-          let firstTextBlock;
-          for (var i = 0; i < arrayOfBlocks.length; i ++) {
-            if (arrayOfBlocks[i]._type === "block" && typeof firstTextBlock === 'undefined') {
-              firstTextBlock = arrayOfBlocks[i];
-            }
-          };
-          let snippetSplit = firstTextBlock.children[0].text.split(" ");
-          let snippet;
-          if (snippetSplit.length > 30) {
-            snippet = snippetSplit;
-            snippet.length = 30;
-            snippet.push(". . .");
-            snippet = snippet.join(" ");
-          } else {
-            snippet = snippetSplit;
-            snippet.push(". . .");
-            snippet = snippet.join(" ");
-          };
-          item.snippet = snippet;
-          item.expanded = false;
-          let dateObj = new Date(item._createdAt);
-          item.dateString = dateObj.toDateString();
-          return item;
-        })
-        this.blogPosts = resultWithSnippet;
-      })
-      .catch(err => {
-        console.log({error: err});
-      })
+    //get first 2 posts
+    this.getNumberOfPosts();
+    this.fetchPosts(0);
   },
   methods: {
     expand: function (id) {
@@ -83,6 +54,59 @@ var app = new Vue({
     },
     debug: function (event, text) {
       console.log(text, event)
-    }
+    },
+    fetchPosts: function (range) {
+      const loRange = range === 0 ? range : range * 2;
+      const hiRange = loRange + 1;
+
+      return fetch (`https://ksrgjbn5.api.sanity.io/v1/data/query/news?query=*[_type == "post"]| order(_createdAt desc)[${loRange}..${hiRange}]`)
+      .then(resBuff => resBuff.json())
+      .then(res => {
+        if (res.result.length === 0) {
+          return;
+        }
+        let resultWithSnippet = res.result.map(item => {
+          let arrayOfBlocks = item.body;
+          let firstTextBlock;
+          for (var i = 0; i < arrayOfBlocks.length; i ++) {
+            if (arrayOfBlocks[i]._type === "block" && typeof firstTextBlock === 'undefined') {
+              firstTextBlock = arrayOfBlocks[i];
+            }
+          };
+          let snippetSplit = firstTextBlock.children[0].text.split(" ");
+          let snippet;
+          if (snippetSplit.length > 30) {
+            snippet = snippetSplit;
+            snippet.length = 30;
+            snippet.push(". . .");
+            snippet = snippet.join(" ");
+          } else {
+            snippet = snippetSplit;
+            snippet.push(". . .");
+            snippet = snippet.join(" ");
+          };
+          item.snippet = snippet;
+          item.expanded = false;
+          let dateObj = new Date(item._createdAt);
+          item.dateString = dateObj.toDateString();
+          return item;
+        });
+        this.blogPosts = this.blogPosts.concat(resultWithSnippet);
+        this.currentFetch = this.currentFetch + 1;
+      })
+      .catch(err => {
+        console.log({error: err});
+      })
+    },
+    getNumberOfPosts: function () {
+      return fetch (`https://ksrgjbn5.api.sanity.io/v1/data/query/news?query=count(*[_type == "post"])`)
+      .then(resBuff => resBuff.json())
+      .then(res => {
+        this.totalNumberOfPosts = res.result;
+      })
+      .catch(err => {
+        console.log({error: err});
+      })
+    } 
   }
 });
